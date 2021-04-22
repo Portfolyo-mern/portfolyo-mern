6fb26b25bee2747a0229686aa96ee09b4f6f5014
akshaymurari <akshaymurari184@gmail.com>
2021-04-22 21:04:48 +0530

    modified scss

--- portfolyo/src/Containers/DashBoard/DashBoard.jsx
+++ portfolyo/src/Containers/DashBoard/DashBoard.jsx
@@ -10,14 +10,15 @@ const DashBoard = () => {
 
   return (
     <>
-      <NavBar/>
+    <NavBar/>
     <div className="dashboardPage">
+
       <div className="dashboarddiv1">
-        <h1 className="dashboardwelcomeh1 text-black">
+        <h1 className="dashboardwelcomeh1">
           Welcome <br />
           {userName}
         </h1>
-        <p className="dashboardwelcomeP text-black">
+        <p className="dashboardwelcomeP">
           You are one step away from creating your beautiful portfolio Click
           below to get started!
         </p>
@@ -30,14 +31,15 @@ const DashBoard = () => {
           <i className="fas fa-arrow-circle-right dashBoardClickHere"></i>
         </NavLink>
       </div>
-      {/* <div className="dashboarddiv2"> */}
-        <video autoPlay loop muted className="">
+      <div className="dashboarddiv2">
+        <video autoPlay loop muted className="dashboardVideo">
           <source
             src="https://player.vimeo.com/external/159035843.sd.mp4?s=0d309dd63ee62d4efc5e0e471824ed7fab0f7f85&profile_id=164&oauth2_token_id=57447761
             "
           ></source>
         </video>
-      {/* </div> */}
+        <div className="videoBlender"></div>
+      </div>
     </div>
     </>
   );
