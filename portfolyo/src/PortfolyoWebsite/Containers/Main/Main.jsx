984f4802f77352cd17ea652a80c83c86f3f7746a
akshaymurari <akshaymurari184@gmail.com>
2021-04-23 14:57:12 +0530

    intialised

---
+++ portfolyo/src/PortfolyoWebsite/Containers/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import "./Main.scss";
+const Main = () => {
+    return (
+        <>
+            <div className="Mainbackground">
+            </div>
+            <Header/>
+        </>
+    )
+}
+
+export default Main
