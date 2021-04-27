e4b1bd79a142033fa347b9278258ddb3a0ace2f9
Deepesh Dragoneel <deepeshash444@gmail.com>
2021-04-27 16:09:11 +0530

    Completed Profilesection Background Selection

--- portfolyo/src/redux/store.jsx
+++ portfolyo/src/redux/store.jsx
@@ -1,23 +1,44 @@
-import {NavbarR,NavbarBg,NavbarIconColor,IconColor,onScrollBg,
-    NavHoverColor,HomeIconText,ArticleIconText,AboutIconText,ContactIconText,NavbarIconText
-} from './reducers';
-import {createStore,combineReducers} from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { logger } from "redux-logger";
 
+import {
+	NavbarR,
+	NavbarBg,
+	NavbarIconColor,
+	IconColor,
+	onScrollBg,
+	NavHoverColor,
+	HomeIconText,
+	ArticleIconText,
+	AboutIconText,
+	ContactIconText,
+	NavbarIconText,
+	profileSectionBackground,
+	encryptBackgroundWords,
+	profileSectionBackgroundColor,
+	AvatarCrop,
+} from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 let red = combineReducers({
-    Navbar:NavbarR,
-    NavbarBg:NavbarBg,
-    NavbarIconColor:NavbarIconColor,
-    IconColor:IconColor,
-    onScrollBg:onScrollBg,
-    NavHoverColor:NavHoverColor,
-    HomeIconText:HomeIconText,
-    ArticleIconText,
-    AboutIconText,
-    ContactIconText,
-    NavbarIconText
+	Navbar: NavbarR,
+	NavbarBg: NavbarBg,
+	NavbarIconColor: NavbarIconColor,
+	IconColor: IconColor,
+	onScrollBg: onScrollBg,
+	NavHoverColor: NavHoverColor,
+	HomeIconText: HomeIconText,
+	ArticleIconText,
+	AboutIconText,
+	ContactIconText,
+	NavbarIconText,
+	profileSectionBackground: profileSectionBackground,
+	encryptBackgroundWords: encryptBackgroundWords,
+	profileSectionBackgroundColor: profileSectionBackgroundColor,
+	AvatarCrop: AvatarCrop,
 });
 
-const Store = createStore(red);
+const Store = createStore(red, composeEnhancers(applyMiddleware(logger)));
 
 export default Store;
