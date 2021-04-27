e45a182de0c0adb3226146944335555e238dd57b
Deepesh Dragoneel <deepeshash444@gmail.com>
2021-04-27 16:09:11 +0530

    Completed Profilesection Background Selection

---
+++ portfolyo/src/PortfolyoWebsite/Components/ProfileSection/AvatarCrop.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import ImageCrop from "./ImageCrop";
+import { connect } from "react-redux";
+
+class AvatarCrop extends Component {
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			userProfilePic: "",
+			editor: null,
+			scaleValue: 1,
+		};
+	}
+
+	handleDrop = (dropped) => {
+		this.setState({ userProfilePic: dropped[0] });
+	};
+
+	setEditorRef = (editor) => this.setState({ editor });
+
+	onCrop = () => {
+		const { editor } = this.state;
+		if (editor !== null) {
+			const url = editor.getImageScaledToCanvas().toDataURL();
+			this.props.dispatch({ type: "profileSectionBackgroundImage", payload: url });
+			this.props.dispatch({ type: "setBackground", payload: 5 });
+			this.setState({ userProfilePic: url });
+		}
+	};
+
+	onScaleChange = (scaleChangeEvent) => {
+		const scaleValue = parseFloat(scaleChangeEvent.target.value);
+		this.setState({ scaleValue });
+	};
+
+	DataURLtoFile = (dataurl, filename) => {
+		let arr = dataurl.split(","),
+			mime = arr[0].match(/:(.*?);/)[1],
+			bstr = atob(arr[1]),
+			n = bstr.length,
+			u8arr = new Uint8Array(n);
+		while (n--) {
+			u8arr[n] = bstr.charCodeAt(n);
+		}
+		return new File([u8arr], filename, { type: mime });
+	};
+
+	profilePicChange = (fileChangeEvent) => {
+		const file = fileChangeEvent.target.files[0];
+		const { type } = file;
+		if (
+			!(
+				type.endsWith("jpeg") ||
+				type.endsWith("png") ||
+				type.endsWith("jpg") ||
+				type.endsWith("gif")
+			)
+		) {
+		} else {
+			this.setState({
+				openCropper: true,
+				selectedImage: fileChangeEvent.target.files[0],
+				fileUploadErrors: [],
+			});
+		}
+	};
+	render() {
+		return (
+			<div style={{
+				width: "100%",
+				textAlign: "center",
+				overflowX: "scroll"
+			}}>
+				<input
+					type="file"
+					name="profilePicBtn"
+					accept="image/png, image/jpeg"
+					onChange={this.profilePicChange}
+					style={{
+						display: "block",
+						margin: "auto"
+					}}
+				/>
+				<ImageCrop
+					imageSrc={this.state.selectedImage}
+					setEditorRef={this.setEditorRef}
+					onCrop={this.onCrop}
+					scaleValue={this.state.scaleValue}
+					onScaleChange={this.onScaleChange}
+					handleDrop={this.handleDrop}
+				/>
+
+				<img src={this.state.userProfilePic} alt="Profile" />
+			</div>
+		);
+	}
+}
+
+export default connect()(AvatarCrop);
--- portfolyo/src/PortfolyoWebsite/Components/ProfileSection/ProfileSection1/ProfileSection1.jsx
+++ portfolyo/src/PortfolyoWebsite/Components/ProfileSection/ProfileSection1/ProfileSection1.jsx
@@ -5,6 +5,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
 import Particles from "react-tsparticles";
 import CreateIcon from "@material-ui/icons/Create";
+import { useSelector, useDispatch } from "react-redux";
+import { useDencrypt } from "use-dencrypt-effect";
+// import Particles from "react-particles-js";
 
 const useStyles = makeStyles({
     cam_icon: {
@@ -17,20 +20,289 @@ const useStyles = makeStyles({
 //profileSection1
 
 const ProfileSection1 = (props) => {
+	const inputRef = useRef();
 
-    const inputRef = useRef();
+	const triggerImageInput = () => {
+		inputRef.current.click();
+	};
 
-    const triggerImageInput = () => {
-      inputRef.current.click();
-    }
+	const [userName, setuserName] = useState("Deepesh Dragoneel");
+	const [tagLine, settagLine] = useState(
+		"oisadhfsdjfnxkjchaiufhdsifhasfhdkjafhlksfjhalkfhaksdjfhnkasjfhaklsfhaslkfhsadfaksjdfhasklfhdsflakfjlksdfhasklfjf"
+	);
+	const [address, setaddress] = useState("Hyderabad, Telangana, India");
+	const classes = useStyles(props);
 
-    const [userName, setuserName] = useState("Deepesh Dragoneel");
-    const [tagLine, settagLine] = useState("oisadhfsdjfnxkjchaiufhdsifhasfhdkjafhlksfjhalkfhaksdjfhnkasjfhaklsfhaslkfhsadfaksjdfhasklfhdsflakfjlksdfhasklfjf");
-    const [address, setaddress] = useState("Hyderabad, Telangana, India");
-    const classes = useStyles(props);
-    return (
+	//redux
+	const selectedBackground = useSelector(
+		(state) => state.profileSectionBackground
+	);
+	const encryptBackgroundWords = useSelector(
+		(state) => state.encryptBackgroundWords
+	);
+	const profileSectionBackgroundColor = useSelector(
+		(state) => state.profileSectionBackgroundColor
+	);
+	const AvatarCrop = useSelector((state) => state.AvatarCrop);
+	const { result, dencrypt } = useDencrypt();
+
+	React.useEffect(() => {
+		let i = 0;
+
+		const action = setInterval(() => {
+			dencrypt(encryptBackgroundWords[i]);
+
+			i = i === encryptBackgroundWords.length - 1 ? 0 : i + 1;
+		}, 2000);
+
+		return () => clearInterval(action);
+	}, []);
+	return (
 		<div className="profileSection1Component">
-			<div className="profileSection1Background">
+			<div
+				className={`profileSection1Background backgroundSelected${selectedBackground}`}
+			>
+				{selectedBackground === 0 ? (
+					<div className="background0div">
+						<div className="purple"></div>
+						<div className="medium-blue"></div>
+						<div className="light-blue"></div>
+						<div className="red"></div>
+						<div className="orange"></div>
+						<div className="yellow"></div>
+						<div className="cyan"></div>
+						<div className="light-green"></div>
+						<div className="lime"></div>
+						<div className="magenta"></div>
+						<div className="lightish-red"></div>
+						<div className="pink"></div>
+						<div id="sqauresinBackground" className="purple"></div>
+						<div
+							id="sqauresinBackground"
+							className="medium-blue"
+						></div>
+						<div
+							id="sqauresinBackground"
+							className="light-blue"
+						></div>
+						<div id="sqauresinBackground" className="red"></div>
+						<div id="sqauresinBackground" className="orange"></div>
+						<div id="sqauresinBackground" className="yellow"></div>
+						<div id="sqauresinBackground" className="cyan"></div>
+						<div
+							id="sqauresinBackground"
+							className="light-green"
+						></div>
+						<div id="sqauresinBackground" className="lime"></div>
+						<div id="sqauresinBackground" className="magenta"></div>
+						<div
+							id="sqauresinBackground"
+							className="lightish-red"
+						></div>
+						<div id="sqauresinBackground" className="pink"></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="purple"
+							style={{
+								backgroundColor: "purple",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="medium-blue"
+							style={{
+								backgroundColor: "medium-blue",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="light-blue"
+							style={{
+								backgroundColor: "light-blue",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="red"
+							style={{
+								backgroundColor: "red",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="orange"
+							style={{
+								backgroundColor: "orange",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="yellow"
+							style={{
+								backgroundColor: "yellow",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="cyan"
+							style={{
+								backgroundColor: "cyan",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="light-green"
+							style={{
+								backgroundColor: "light-green",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="lime"
+							style={{
+								backgroundColor: "lime",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="magenta"
+							style={{
+								backgroundColor: "purple",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="lightish-red"
+							style={{
+								backgroundColor: "purple",
+							}}
+						></div>
+						<div
+							id="sqauresinBackgroundInside"
+							className="pink"
+							style={{
+								backgroundColor: "purple",
+							}}
+						></div>
+					</div>
+				) : selectedBackground === 1 ? (
+					<div class="animation-wrapper">
+						<div class="particle particle-1"></div>
+						<div class="particle particle-2"></div>
+						<div class="particle particle-3"></div>
+						<div class="particle particle-4"></div>
+					</div>
+				) : selectedBackground === 3 ? (
+					<Particles
+						id="tsparticles"
+						options={{
+							background: {
+								color: {
+									value: "#0d47a1",
+								},
+							},
+							fpsLimit: 80,
+							interactivity: {
+								detectsOn: "canvas",
+								events: {
+									onClick: {
+										enable: true,
+										mode: "push",
+									},
+									onHover: {
+										enable: true,
+										mode: "repulse",
+									},
+									resize: true,
+								},
+								modes: {
+									bubble: {
+										distance: 200,
+										duration: 2,
+										opacity: 0.8,
+										size: 40,
+									},
+									push: {
+										quantity: 4,
+									},
+									repulse: {
+										distance: 200,
+										duration: 0.2,
+									},
+								},
+							},
+							particles: {
+								color: {
+									value: "#ffffff",
+								},
+								links: {
+									color: "#ffffff",
+									distance: 150,
+									enable: true,
+									opacity: 0.5,
+									width: 1,
+								},
+								collisions: {
+									enable: true,
+								},
+								move: {
+									direction: "none",
+									enable: true,
+									outMode: "bounce",
+									random: false,
+									speed: 3,
+									straight: false,
+								},
+								number: {
+									density: {
+										enable: true,
+										value_area: 800,
+									},
+									value: 80,
+								},
+								opacity: {
+									value: 0.5,
+								},
+								shape: {
+									type: "circle",
+								},
+								size: {
+									random: true,
+									value: 2,
+								},
+							},
+							detectRetina: true,
+						}}
+					/>
+				) : selectedBackground === 2 ? (
+					<div className="profileSectionBackground2">
+						<span
+							style={{
+								alignSelf: "center",
+								verticalAlign: "middle",
+							}}
+						>
+							{result}
+						</span>
+					</div>
+				) : selectedBackground === 4 ? (
+					<div
+						className="profileSectionBackground4"
+						style={{
+							backgroundColor: profileSectionBackgroundColor,
+						}}
+					></div>
+				) : (
+					<div className="profileSectionBackground5" style={{
+						backgroundImage: {AvatarCrop}
+					}}>
+						<img
+							className="profileSectionBackground5Image"
+							src={AvatarCrop}
+						></img>
+					</div>
+				)}
+
 				<CameraAltIcon
 					style={{
 						fontSize: "2rem",
