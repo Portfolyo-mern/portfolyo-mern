388da6b452d0e06853558e69d5c15213f23c9845
DeepeshDragoneel <deepeshash444@gmail.com>
2021-08-25 15:10:35 +0530

    Fixed Profile pIc

--- portfolyo/src/PortfolyoWebsite/Components/ProfileSection/EditProfilePic/ImageCrop.jsx
+++ portfolyo/src/PortfolyoWebsite/Components/ProfileSection/EditProfilePic/ImageCrop.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AvatarEditor from 'react-avatar-editor';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import PropTypes from "prop-types";
+import AvatarEditor from "react-avatar-editor";
+import Button from "@material-ui/core/Button";
 // import Typography from '@material-ui/core/Typography';
 // import Slider from '@material-ui/core/Slider';
 // import IconButton from '@material-ui/core/IconButton';
 // import Tooltip from '@material-ui/core/Tooltip';
 // import Dropzone from 'react-dropzone';
-
 
 const ImageCrop = ({
     imageSrc,
@@ -20,36 +19,49 @@ const ImageCrop = ({
         <Button
             variant="contained"
             className="mb-3 ml-3 bg-dark text-white"
-            style={{ display: "inline-block" }}
+            style={{
+                display: "inline-block",
+                position: "relative",
+                top: "9px",
+            }}
             onClick={onCrop}
             component="span"
         >
-            Upload
+            Save
         </Button>
         <div className="editorOverlayInner">
             <div className="editorModalContent clearfix">
                 <div className="cropCnt">
                     <AvatarEditor
                         image={imageSrc}
-                        border={50}
-                        style={{ height: "17rem", width: "17rem" }}
+                        border={15}
+                        style={{ height: "14rem", width: "14rem" }}
                         scale={scaleValue}
                         rotate={0}
                         ref={setEditorRef}
                         className="cropCanvas"
                     />
-
-                    <input
-                        style={{ width: "100%" }}
-                        class="custom-range"
-                        type="range"
-                        value={scaleValue}
-                        name="points"
-                        min="1"
-                        max="10"
-                        step={1}
-                        onChange={onScaleChange}
-                    />
+                    <div
+                        className="profilePicZoomDiv"
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <p style={{ margin: "1rem"}}>Zoom: </p>
+                        <input
+                            style={{ width: "100%" }}
+                            class="custom-range"
+                            type="range"
+                            value={scaleValue}
+                            name="points"
+                            min="1"
+                            max="10"
+                            step={1}
+                            onChange={onScaleChange}
+                        />
+                    </div>
                     {/* <button onClick={onCrop} className="editorOverlayCloseBtn crpBtn">
               Save
             </button> */}
@@ -60,11 +72,11 @@ const ImageCrop = ({
 );
 
 ImageCrop.propTypes = {
-  open: PropTypes.bool.isRequired,
-  setEditorRef: PropTypes.func.isRequired,
-  onCrop: PropTypes.func.isRequired,
-  scaleValue: PropTypes.number.isRequired,
-  onScaleChange: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
+    setEditorRef: PropTypes.func.isRequired,
+    onCrop: PropTypes.func.isRequired,
+    scaleValue: PropTypes.number.isRequired,
+    onScaleChange: PropTypes.func.isRequired,
 };
 
 export default ImageCrop;
