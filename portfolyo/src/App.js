63e3ca45f0ed4848d6c81f2ac2206f65405d8953
akshaymurari <akshaymurari184@gmail.com>
2021-09-27 21:49:55 +0530

    changed baseurl

--- portfolyo/src/App.js
+++ portfolyo/src/App.js
@@ -51,6 +51,6 @@ function App() {
 
 export default App;
 
-export const Baseurl = "http://167.71.235.21:3001";
+export const Baseurl = "https://167.71.235.21:3001";
 
 export const domain = "https://portfolyo.studio/#/"
