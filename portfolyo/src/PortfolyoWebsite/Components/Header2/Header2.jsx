f91968721db1877adc2577eb2e1568fbbf201fb0
DeepeshDragoneel <deepeshash444@gmail.com>
2021-09-28 12:11:48 +0530

    Modified View Option

--- portfolyo/src/PortfolyoWebsite/Components/Header2/Header2.jsx
+++ portfolyo/src/PortfolyoWebsite/Components/Header2/Header2.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 // import { makeStyles } from '@material-ui/core/styles';
 // import AppBar from '@material-ui/core/AppBar';
 // import Toolbar from '@material-ui/core/Toolbar';
@@ -7,7 +7,7 @@ import React,{useState} from 'react'
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 import "./Header2.scss";
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 // const useStyles = makeStyles((theme) => ({
 //     root: {
 //         flexGrow: 1,
@@ -21,80 +21,115 @@ import {useSelector} from 'react-redux';
 // }));
 
 let Header2 = (props) => {
-    const [hover,sethover] = useState(false);
-    const Navbarbg = useSelector(state=>state.NavbarBg);
-    const NavbarIconColor = useSelector(state=>state.NavbarIconColor);
-    const IconColor = useSelector(state=>state.IconColor);
-    const onScrollBg = useSelector(state=>state.onScrollBg);
-    const NavHoverColor = useSelector(state=>state.NavHoverColor);
-    const HomeIconText = useSelector(state=>state.HomeIconText);
-    const ArticleIconText = useSelector(state=>state.ArticleIconText);
-    const AboutIconText = useSelector(state=>state.AboutIconText);
-    const ContactIconText = useSelector(state=>state.ContactIconText);
-    const NavbarIconText = useSelector(state=>state.NavbarIconText);
-    const ProjectIconText = useSelector(state=>state.ProjectIconText);
-    
-    props.menu[0].name=AboutIconText;
-    props.menu[1].name=HomeIconText;
-    props.menu[2].name=ProjectIconText;
-    props.menu[3].name=ArticleIconText;
-    props.menu[4].name=ContactIconText;
+    const [hover, sethover] = useState(false);
+    const Navbarbg = useSelector((state) => state.NavbarBg);
+    const NavbarIconColor = useSelector((state) => state.NavbarIconColor);
+    const IconColor = useSelector((state) => state.IconColor);
+    const onScrollBg = useSelector((state) => state.onScrollBg);
+    const NavHoverColor = useSelector((state) => state.NavHoverColor);
+    const HomeIconText = useSelector((state) => state.HomeIconText);
+    const ArticleIconText = useSelector((state) => state.ArticleIconText);
+    const AboutIconText = useSelector((state) => state.AboutIconText);
+    const ContactIconText = useSelector((state) => state.ContactIconText);
+    const NavbarIconText = useSelector((state) => state.NavbarIconText);
+    const ProjectIconText = useSelector((state) => state.ProjectIconText);
 
+    props.menu[0].name = AboutIconText;
+    props.menu[1].name = HomeIconText;
+    props.menu[2].name = ProjectIconText;
+    props.menu[3].name = ArticleIconText;
+    props.menu[4].name = ContactIconText;
 
     // console.log(NavbarIconColor);
     const [colorChange, setColorchange] = useState(false);
-    const changeNavbarColor = () =>{
-        if(window.scrollY >= 80){
+    const changeNavbarColor = () => {
+        if (window.scrollY >= 80) {
             setColorchange(true);
-       }
-       else{
-         setColorchange(false);
-       }
+        } else {
+            setColorchange(false);
+        }
     };
-    window.addEventListener('scroll', changeNavbarColor);
+    window.addEventListener("scroll", changeNavbarColor);
     // const classes = useStyles();
     // console.log(props.menu)
     return (
         <div className="Header2 m-0">
-            <nav id="scrollNavbar" class="navbar my-0 py-2 navbar-fixed-top  navbar-expand-lg navbar-dark fixed-top" style={{ postion: "sticky", background: (colorChange)?onScrollBg:Navbarbg,color:NavbarIconColor,width:'100%' }} >
-            {/* eslint-disable-next-line */}
-                <a class="navbar-brand t ml-2" style={{ color:NavbarIconColor,cursor:"pointer" }} onClick={()=>{
-                    props.func.ScrollHome();
-                }}>{NavbarIconText}</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <nav
+                id="scrollNavbar"
+                class="navbar my-0 py-2 navbar-fixed-top  navbar-expand-lg navbar-dark fixed-top"
+                style={{
+                    postion: "sticky",
+                    background: colorChange ? onScrollBg : Navbarbg,
+                    color: NavbarIconColor,
+                    width: "85%",
+                }}
+            >
+                {/* eslint-disable-next-line */}
+                <a
+                    class="navbar-brand t ml-2"
+                    style={{ color: NavbarIconColor, cursor: "pointer" }}
+                    onClick={() => {
+                        props.func.ScrollHome();
+                    }}
+                >
+                    {NavbarIconText}
+                </a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto py-0 my-0" >
+                    <ul class="navbar-nav ml-auto py-0 my-0">
                         {props.menu.map((ele) => (
-                            <li style={{cursor:"pointer"}} class="nav-item  mr-4 active" onClick={ () => {
-                                          if(ele.to==="education"){
-                                              props.func.ScrollE();
-                                          }
-                                          if(ele.to==="skills"){
-                                              props.func.ScrollH();
-                                          }
-                                          if(ele.to==="about"){
-                                            props.func.ScrollA();
-                                          }
-                                          if(ele.to==="project"){
-                                            props.func.ScrollP();
-                                          }
-                                          if(ele.to==="contactform"){
-                                              props.func.ScrollC();
-                                          }
-                                      }
-                                      }>
-                                          {/* eslint-disable-next-line */}
-                                <a class="nav-link stroke  text-center" onMouseEnter={()=>sethover(true)} onMouseLeave={()=>sethover(false)}  style={{color:(hover)?NavHoverColor:IconColor,
-                                         }} >{ele.name} <span class="sr-only">(current)</span></a>
+                            <li
+                                style={{ cursor: "pointer" }}
+                                class="nav-item  mr-4 active"
+                                onClick={() => {
+                                    if (ele.to === "education") {
+                                        props.func.ScrollE();
+                                    }
+                                    if (ele.to === "skills") {
+                                        props.func.ScrollH();
+                                    }
+                                    if (ele.to === "about") {
+                                        props.func.ScrollA();
+                                    }
+                                    if (ele.to === "project") {
+                                        props.func.ScrollP();
+                                    }
+                                    if (ele.to === "contactform") {
+                                        props.func.ScrollC();
+                                    }
+                                }}
+                            >
+                                {/* eslint-disable-next-line */}
+                                <a
+                                    class="nav-link stroke  text-center"
+                                    onMouseEnter={() => sethover(true)}
+                                    onMouseLeave={() => sethover(false)}
+                                    style={{
+                                        color: hover
+                                            ? NavHoverColor
+                                            : IconColor,
+                                    }}
+                                >
+                                    {ele.name}{" "}
+                                    <span class="sr-only">(current)</span>
+                                </a>
                             </li>
                         ))}
                     </ul>
                 </div>
             </nav>
         </div>
-    )
-}
+    );
+};
 
-export default Header2
+export default Header2;
