f8b75873f87cee1737ee2dec251535045abe2e65
akshaymurari <akshaymurari184@gmail.com>
2021-10-14 09:08:49 +0530

    fixed minor bugs

--- portfolyo/src/Home/Home/works.js
+++ portfolyo/src/Home/Home/works.js
@@ -36,7 +36,7 @@ class AppWorks extends React.Component {
             footer={null}
             destroyOnClose = {true}
           >
-          <iframe width="100%" height="350" src="https://www.youtube.com/embed/Bs4w_o07wEo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="100%" height="350" src="https://www.youtube.com/embed/fvXABkXucr0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             
           </Modal>
         </div>
